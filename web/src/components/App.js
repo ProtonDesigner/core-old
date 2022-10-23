e2af7312bca106cbae852adc73a6e424974e2dc0
TechStudent11 <76978184+TechStudent10@users.noreply.github.com>
2022-10-22 18:06:03 -0600

    added some hiearchy stuffs (unfinished as of this moment)

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./App.scss"
 import Preview from './Preview';
 import Rightbar from './Rightbar';
@@ -6,10 +6,21 @@ import Sidebar from './Sidebar';
 import Topbar from './Topbar';
 
 export default function App(props) {
+    const [elements, setElements] = useState([
+        {
+            "id": 1,
+            "name": "Text"
+        },
+        {
+            "id": 2,
+            "name": "Image"
+        }
+    ]);
+
     return (
         <div className="app">
             <Topbar />
-            <Sidebar />
+            <Sidebar elements={elements} />
             <Rightbar />
             <Preview />
         </div>
--- web/src/components/Sidebar.js
+++ web/src/components/Sidebar.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import "./Sidebar.scss"
 
+const Element = (props) => {
+    return <div className="sidebar__element">
+        <h4>{props.element_name}</h4>
+    </div>
+}
+
 export default function Sidebar(props) {
     return (
         <div className="sidebar">
             <h3 className="sidebar__title">Hierarchy</h3>
+            <br />
+            {props.elements && props.elements.map((element) => {
+                return <>
+                    <Element key={element.id} element_name={element.name} />
+                    <div style={{
+                        width: '100%',
+                        height: '2.5px',
+                        backgroundColor: 'black',
+                        paddingTop: '5px',
+                        paddingBottom: '5px'
+                    }} />
+                </>
+            })}
         </div>
     )
 }
